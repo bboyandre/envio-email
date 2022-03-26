8e35d86207d6089847a3b50aebc97023e8e8fb00
Andre Felipe <andre.felipe@novam3.com>
2022-03-26 11:21:59 -0300

    ajuste route

--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [RegisterController::class, 'showform']);
-});
+Route::get('/', function () {
+    return view('pages.index');
+})->name('showform');
